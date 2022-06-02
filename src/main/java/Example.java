66eed9fe5a3770b130e2cec762429e59f40759fa
surisettyvinayvyas <106443657+surisettyvinayvyas@users.noreply.github.com>
2022-06-02 19:46:45 +0530

    Update Example.java

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -9,7 +9,7 @@ public class Example {
 
     @RequestMapping("/")
     String home() {
-        return "Hello World - vinayvyassurisetty!";
+        return "Hello World - vinayvy!";
     }
 
     public static void main(String[] args) throws Exception {
