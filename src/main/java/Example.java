148b6a73435604df5008a9aaa439f8e495becb28
Sabarinathan S <ss00927570@techmahindra.com>
2023-01-13 06:40:16 +0000

    Demo test

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -9,7 +9,7 @@ public class Example {
 
     @RequestMapping("/")
     String home() {
-        return "Hello World - v3!";
+        return "Hello World - v4!";
     }
 
     public static void main(String[] args) throws Exception {
